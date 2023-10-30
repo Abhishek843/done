182fb26dfb2af89bfc1b5b935c2117498995fffc
Abhishek843 <82261167+Abhishek843@users.noreply.github.com>
2023-10-30 16:54:45 +0530

    Update password.js

--- controller/password.js
+++ controller/password.js
@@ -55,7 +55,7 @@ exports.postForgotPassword = async (req, res) => {
             to: receivers,
             subject: 'Password reset link',
             htmlContent: `
-                <a href="http://23.21.28.1:3200/password/reset-password/${id}" target="_blank">
+                <a href="http://35.153.73.136:3200/password/reset-password/${id}" target="_blank">
                     Click here to reset password
                 </a>
             `
