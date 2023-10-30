00aa6f5dc77e1ce596836784cb5721ca52c2f7fb
Abhishek843 <82261167+Abhishek843@users.noreply.github.com>
2023-10-30 16:49:33 +0530

    Update forgotPassword.js

--- public/js/forgotPassword.js
+++ public/js/forgotPassword.js
@@ -10,7 +10,7 @@ function forgotPassword(e){
     const user = {
         email: emailInput.value
     };
-    axios.post(`http://23.21.28.1:3200/password/forgot-password`, user)
+    axios.post(`http://35.153.73.136:3200/password/forgot-password`, user)
     .then((res) => {
         const msg = res.data.msg;
         showSuccessInDOM(msg);
